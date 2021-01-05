977cdb53495d93b6563e657a4171185c9581b2f3
大同 <372354984@qq.com>
2021-01-05 19:38:38 +0800

    文章基础框架

---
+++ pages/login/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="auth-page">
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-xs-12">
+          <h1 class="text-xs-center">Sign {{ isLogin ? 'in' : 'up' }}</h1>
+          <p class="text-xs-center">
+            <nuxt-link v-if="isLogin" to="/register">need an account?</nuxt-link>
+            <nuxt-link v-if="!isLogin" to="/login">Have an account?</nuxt-link>
+          </p>
+
+          <ul class="error-messages">
+            <li>That email is already taken</li>
+          </ul>
+
+          <form>
+            <fieldset v-if="!isLogin" class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Your Name"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="text"
+                placeholder="Email"
+              />
+            </fieldset>
+            <fieldset class="form-group">
+              <input
+                class="form-control form-control-lg"
+                type="password"
+                placeholder="Password"
+              />
+            </fieldset>
+            <button class="btn btn-lg btn-primary pull-xs-right">
+              Sign {{ isLogin ? 'in' : 'up' }}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginIndex',
+  computed: {
+    isLogin() { 
+      return this.$route.name === 'login'
+    },
+  },
+}
+</script>
+
+<style>
+</style>
